56e5cd28a4bf3947e055d2343715d4fd8b8dc9c5
Dorian HODIN <dorian.hodin@gmail.com>
2023-01-25 15:05:46 +0100

    Mise à jour de 'Sources/php_script/script/config.php'

--- Sources/php_script/script/config.php
+++ Sources/php_script/script/config.php
@@ -10,7 +10,14 @@ function connection(): bool|int|mysqli
 
 
 
-    echo $_SERVER['REMOTE_ADDR'];
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    echo $ip;
 
     try {                                                                //Try to connect to the database
 
