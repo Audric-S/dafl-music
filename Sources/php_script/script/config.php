41d1e8dbb3f9a30cb0a3e28a72d682e3b74425d8
Dorian HODIN <dorian.hodin@gmail.com>
2023-01-25 15:09:08 +0100

    Mise à jour de 'Sources/php_script/script/config.php'

--- Sources/php_script/script/config.php
+++ Sources/php_script/script/config.php
@@ -18,15 +18,3 @@ function connection(): bool|int|mysqli
 
     }
 }
-
-if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-        $ip = $_SERVER['HTTP_CLIENT_IP'];
-    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    } else {
-        $ip = $_SERVER['REMOTE_ADDR'];
-    }
-    echo "AH";
-    echo $ip;
-
-
